6483cabc0fdb88913fbe1e27328f3a6098cf133a
doluvor <doluvor@126.com>
2015-09-18 11:19:59 -0400

    Change db to mysql, decrypt aes data

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -6,13 +6,34 @@ class ScoresController < ApplicationController
   end
 
   def index
-  	@scores = Score.all
+  	@scores = Score.last(60)
+
+	#score = Score.last
+	#decipher = OpenSSL::Cipher::AES.new(128, :CBC)
+        #decipher.decrypt
+        #decipher.key = "this_is_key_1234"
+        #decipher.iv  = "this_is_iv_12345"
+
+        #decipher.padding = 0
+	#data = Base64.decode64(URI.unescape(score.zone));
+        #deciphered = decipher.update(data) + decipher.final
+
+        #render plain: deciphered, status: :ok
   end
 
   def createPlain
   	score = Score.create dps:params[:dps], player:params[:player], duration:params[:duration], durationS:params[:durationS], zone:params[:zone], enemy:params[:enemy], job:params[:job], charaID:params[:charaID]
 
-  	render json: score, status: :ok
+	decipher = OpenSSL::Cipher::AES.new(128, :CBC)
+	decipher.decrypt
+	decipher.key = "this_is_key_1234"
+	decipher.iv  = "this_is_iv_12345"
+
+	decipher.padding = 7
+
+	#deciphered = decipher.update() + decipher.final
+
+  	render plain: "1", status: :ok
   end
 
   def create
@@ -20,14 +41,19 @@ class ScoresController < ApplicationController
 
   	decipher = OpenSSL::Cipher::AES.new(128, :CBC)
   	decipher.decrypt
-  	decipher.key = "this is key 12345"
-  	#decipher.iv = iv
+  	decipher.key = "this_is_key_1234"
+  	decipher.iv = "this_is_iv_12345"
 
-  	deciphered = decipher.update(params[:encrypted]) + decipher.final
+	#decipher.padding = 0
+
+	data = Base64.decode64(URI.unescape(request.raw_post));
+  	deciphered = decipher.update(data) + decipher.final
 
   	scoreData = JSON.parse deciphered
 
-  	render plain: "#{scoreData["player"]} -- #{params[:encrypted]}", status: :ok
+	score = Score.create dps:scoreData["dps"], player:scoreData["player"], duration:scoreData["duration"], durationS:scoreData["durationS"], zone:scoreData["zone"], enemy:scoreData["enemy"], charaID:scoreData["charaID"], job:scoreData["job"]
+
+  	render json: score, status: :ok
   end
 
   def testGet
